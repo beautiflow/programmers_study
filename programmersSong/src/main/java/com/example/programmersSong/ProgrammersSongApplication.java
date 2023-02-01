b07248c9cd9a2ba18354632244884a863cd4decf
beautiflow <1114ny@naver.com>
2023-02-01 11:10:20 +0900

    programmersSong Project

---
+++ programmersSong/src/main/java/com/example/programmersSong/ProgrammersSongApplication.java
@@ -0,0 +1,13 @@
+package com.example.programmersSong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammersSongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammersSongApplication.class, args);
+	}
+
+}
---
+++ programmersSong/src/test/java/com/example/programmersSong/ProgrammersSongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programmersSong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammersSongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
